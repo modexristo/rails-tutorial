7f30447aab89a7c7b74f23e319133fd4d14e3a17
Chris Ag <cagoras@sunsoft.gr>
2014-06-19 19:54:28 +0300

    add articles#create and a form to create a new form in the /article/new route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def new
 
   end
+
+  # the params[] object is the one that references the parameters returned from the form,
+  # which happens to be a hash that models the resource.
+  # I suppose that the key is the name of the resource (:article in this instance) and the
+  # value is a hash of its attributes (in this example it is {title: "", text: ""})
+  def create
+    render plain: params[:article].inspect
+  end
 end

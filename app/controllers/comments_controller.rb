508cfc568c59e42f66c61148be52a3f32853f3a9
Chris Ag <cagoras@sunsoft.gr>
2014-06-23 14:19:15 +0300

    Add Comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+
+  # the params returned from the form in the articles/show.html.erb
+  # will be sth like:
+  # params[] = {
+  #   :controller => ''
+  #   :action => ''
+  #   :id = '1'
+  #   :article => {
+  #     :title => "title"
+  #     :text => "text"
+  #     :comment = { :commenter => "comm_id", :body => "comment"}
+  #
+  #   }
+  # }
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def comment_params
+    # why 'comment' and not 'comments' ?
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+end
